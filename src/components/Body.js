59e8917e0310b0ce364d05bb1f7e68fd19e2316a
Anees <aneesnafeesimmu@gmail.com>
2024-05-10 23:52:53 +0530

    episode-6(1)

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,22 @@
 import Card from "./Card"
-import resList from "../utils/resData"
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
-    const [listItems, setListItems] = useState(resList);
+    const [listItems, setListItems] = useState([]);
+    useEffect(()=>{
+        console.log("Effect rendered");
+        fetchData();
+    }, [])
+    const fetchData = async() => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.6287557&lng=79.4191795&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        // console.log(data);
+        const jsonData = await data.json();
+        // console.log(jsonData);
+        setListItems(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
+    if(listItems.length === 0){
+        return <Shimmer />
+    }
     return (
         <div className="body">
             <div className="search" style={{padding: "10px"}}>Search</div>
@@ -15,6 +29,7 @@ const Body = () => {
                     console.log(filteredItems);
                 }}>Top Rated Restuarents</button>
             </div>
+            {console.log(listItems.length)}
             <div className="card-container">
                 {listItems.map((rest) => (
                     <Card key={rest.info.id} resData = {rest}/>
