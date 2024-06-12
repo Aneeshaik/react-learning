52c4f02ba9899e94bb09fc154eddcf071976afc3
Anees <aneesnafeesimmu@gmail.com>
2024-06-12 22:35:45 +0530

    episode-11

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,10 +1,15 @@
+import { IMG_URL } from "../utils/constants";
+
 const ItemList = ({item}) => {
     return(
         <div className="">
             {item?.itemCards.map((item) => (
                 <div key={item?.card?.info?.id} className="border-b-2 pb-1 text-left">
-                    <span className="font-semibold">{item?.card?.info?.name}</span>
+                    <div>
+                        <span className="font-semibold">{item?.card?.info?.name}</span>
+                    </div>
                     <p>{item?.card?.info?.description}</p>
+                    {/* <img className="w-32 rounded-md" alt="food-item" src={IMG_URL + item?.card?.info?.imageId} /> */}
                 </div>
             ))}
         </div>
